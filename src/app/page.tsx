681b8c5deec1cfcd78576a7ded59033995996721
lgweida <45736060+lgweida@users.noreply.github.com>
2024-01-14 01:52:51 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-   
-      <div>
-        <p className='text-5xl text-red-900'>
-          Hello world????
-        </p>
-      </div>
-  )
+    <div>
+      <p className="text-5xl text-red-900">Hello world?</p>
+    </div>
+  );
 }
